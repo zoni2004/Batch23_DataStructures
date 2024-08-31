ec5d3392f983f80f28b2fef6b1818e35c33f59fc
zoni2004 <142867557+zoni2004@users.noreply.github.com>
2024-08-31 12:34:07 +0500

    Create TASK.h

---
+++ LAB2/TASK2/TASK.h
@@ -0,0 +1,7 @@
+#ifndef matrixMultiply_H
+#define matrixMultiply_H
+
+// Multiplies two matrices A and B and stores the result in matrix C
+void multiplyMatrices(const int A[][10], const int B[][10], int C[][10], int m, int n, int p);
+
+#endif // MATRIX_MULTIPLY_H
